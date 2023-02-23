b6539ac2ed823faa89bcfd3b4c6a58358ee3f847
amartiniuc <amartiniuc@griddynamics.com>
2023-02-23 12:52:25 +0200

    add animation

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,4 +1,5 @@
 import PropTypes from "prop-types";
+import { motion, AnimatePresence } from "framer-motion";
 import { FeedbackItem } from "./FeedbackItem";
 
 export const FeedbackList = ({ feedback, handleDelete }) => {
@@ -7,15 +8,24 @@ export const FeedbackList = ({ feedback, handleDelete }) => {
   }
   return (
     <div className="feedback-list">
-      {feedback.map((item) => {
-        return (
-          <FeedbackItem
-            key={item.id}
-            item={item}
-            handleDelete={handleDelete}
-          />
-        );
-      })}
+      <AnimatePresence>
+        {feedback.map((item) => {
+          return (
+            <motion.div
+              key={item.id}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+            >
+              <FeedbackItem
+                key={item.id}
+                item={item}
+                handleDelete={handleDelete}
+              />
+            </motion.div>
+          );
+        })}
+      </AnimatePresence>
     </div>
   );
 };
